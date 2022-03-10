1e0c9cfdac13b9a020f8a8337cead6a8d5ba814e
Bilal Kapadia <P2592134@my365.dmu.ac.uk>
2022-03-10 13:58:31 +0000

    made changes for week 21 however I am stuck on ending part

--- AdminSystem/StockConfirmDelete.aspx.cs
+++ AdminSystem/StockConfirmDelete.aspx.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance clsStockList
+        clsStockList aStock = new clsStockList();
+        //get data from from session object
+        aStock = (clsStockList)Session["aStock"];
+        //display the Vehicle Id for this entry
+        Response.Write(aStock.Vmodel);
+    }
 
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStockList
+        clsStockList aStock = new clsStockList();
+        //capture the Vehicle Id
+        aStock.Vmodel = txtVehicleNo.Text;
+        //store the Vehicle Id in the session object
+        Session["aStock"] = aStock;        
+        //navigate to viewer page
+        Response.Redirect("StockConfirmDelete.aspx");
     }
 }
