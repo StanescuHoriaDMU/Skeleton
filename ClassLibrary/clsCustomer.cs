9a00b444d0727cdd5d81a9b7b63439963eb6c776
StanescuHoria <horiabvandrei@yahoo.com>
2022-03-11 14:25:37 +0000

    Added properties for customer class + tests for each one

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,9 +1,18 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
         public clsCustomer()
         {
         }
+
+        public bool VerifiedCustomer { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string EmailAdress { get; set; }
+        public string Username { get; set; }
     }
 }
--- Testing2/UnitTest1.cs
+++ Testing2/UnitTest1.cs
@@ -5,13 +5,61 @@ using System;
 namespace Testing2
 {
     [TestClass]
-    public class UnitTest1
+    public class tstCustomer
     {
         [TestMethod]
         public void InstanceOK()
         {
             clsCustomer ACustomer = new clsCustomer();
             Assert.IsNotNull(ACustomer);
+        }
+        [TestMethod]
+        public void VerifiedCustomerPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean TestData = true;
+            ACustomer.VerifiedCustomer = TestData;
+            Assert.AreEqual(ACustomer.VerifiedCustomer, TestData);
+        }
+        [TestMethod]
+        public void DateOfBirthPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            DateTime TestData = DateTime.Now.Date;
+            ACustomer.DateOfBirth = TestData;
+            Assert.AreEqual(ACustomer.DateOfBirth, TestData);
+        }
+        [TestMethod]
+        public void UserIdPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            int TestData = 1;
+            ACustomer.UserId = TestData;
+            Assert.AreEqual(ACustomer.UserId, TestData);
+        }
+        [TestMethod]
+        public void FullNamePropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "Ana Maria";
+            ACustomer.FullName = "Ana Maria";
+            Assert.AreEqual(ACustomer.FullName, TestData);
+        }
+        [TestMethod]
+        public void EmailAdressPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "anamaria@yahoo.com";
+            ACustomer.EmailAdress = TestData;
+            Assert.AreEqual(ACustomer.EmailAdress, TestData);
+        }
+        [TestMethod]
+        public void UsernamePropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "anamaria21";
+            ACustomer.Username = TestData;
+            Assert.AreEqual(ACustomer.Username, TestData);
         }
     }
 }
