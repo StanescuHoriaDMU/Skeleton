8d7c1a082d28c80f5c8185ab268967d77de24996
StanescuHoria <horiabvandrei@yahoo.com>
2022-03-11 19:54:16 +0000

    Added the simple presentation layer

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        clsCustomer ACustomer = new clsCustomer();
+        ACustomer.UserId = int.Parse(txtUserId.Text);
+        ACustomer.FullName = txtFullName.Text;
+        ACustomer.EmailAdress = txtEmailAdress.Text;
+        ACustomer.DateOfBirth = DateTime.Parse(txtDateOfBirth.Text);
+        ACustomer.Username = txtUsername.Text;
+        ACustomer.VerifiedCustomer = checkboxVerifiedCustomer.Checked;
+        Session["ACustomer"] = ACustomer;
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
