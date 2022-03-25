4201e9f2a8aee030f186ad1573c3de08ae1a9a24
StanescuHoria <horiabvandrei@yahoo.com>
2022-03-25 01:21:54 +0000

    Created and finished collection class

--- AdminSystem/CustomerList.aspx
+++ AdminSystem/CustomerList.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:ListBox ID="lstCustomerList" runat="server" Height="354px" Width="382px"></asp:ListBox>
     </form>
 </body>
 </html>
---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        private List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        public List<clsCustomer> CustomerList {
+            get
+            {
+                return mCustomerList;
+            }
+            set 
+            {
+                mCustomerList = value;
+            } 
+        }
+        public int Count { 
+            get
+            {
+                return mCustomerList.Count;
+            }
+            set
+            {
+                
+            }
+        }
+        public clsCustomer ThisCustomer { get; set; }
+        public clsCustomerCollection()
+        {
+            int Index = 0;
+            int RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_CustomerManagement_SelectAll");
+            RecordCount = DB.Count;
+            while(Index < RecordCount)
+            {
+                clsCustomer ACustomer = new clsCustomer();
+                ACustomer.UserId = Convert.ToInt32(DB.DataTable.Rows[0]["userId"]);
+                ACustomer.FullName = Convert.ToString(DB.DataTable.Rows[0]["FullName"]);
+                ACustomer.EmailAdress = Convert.ToString(DB.DataTable.Rows[0]["EmailAdress"]);
+                ACustomer.DateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[0]["DateOfBirth"]);
+                ACustomer.Username = Convert.ToString(DB.DataTable.Rows[0]["Username"]);
+                ACustomer.VerifiedCustomer = Convert.ToBoolean(DB.DataTable.Rows[0]["VerifiedCustomer"]);
+                mCustomerList.Add(ACustomer);
+                Index++;
+            }
+        }
+    }
+}
