54ac8461108a71906c484f09d6a0ac5f9a3d07b4
Muhammad Areeb Farhan <areeb@salesassist.pk>
2022-03-26 09:52:39 +0500

    ordersTest Class created

---
+++ Testing6/clsOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Testing6
+{
+    public class clsOrder
+    {
+        internal DateTime soldDate;
+
+        public int purchaseID { get; internal set; }
+        public int userID { get; internal set; }
+        public int vehicleID { get; internal set; }
+        public bool dispatched { get; internal set; }
+        public string userAddress { get; internal set; }
+    }
+}
---
+++ Testing6/tstOrders.cs
@@ -0,0 +1,75 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing6
+{
+    [TestClass]
+    public class tstOrders
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Assert.IsNotNull(AnOrder);
+        }
+
+        [TestMethod]
+        public void PurchaseIDPropertyOk()
+        {
+            clsOrder AnOrder =new clsOrder();
+            int TestData=int.MaxValue;
+            AnOrder.purchaseID = TestData;
+            Assert.AreEqual(AnOrder.purchaseID, TestData);
+        }
+
+        [TestMethod]
+        public void UserIDPropertyOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = int.MaxValue;
+            AnOrder.userID = TestData;
+            Assert.AreEqual(AnOrder.userID, TestData);
+        }
+
+        [TestMethod]
+        public void VehicleIDPropertyOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = int.MaxValue;
+            AnOrder.vehicleID = TestData;
+            Assert.AreEqual(AnOrder.vehicleID, TestData);
+        }
+
+        [TestMethod]
+        public void DispatchedPropertyOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.dispatched = TestData;
+            Assert.AreEqual(AnOrder.dispatched, TestData);
+        }
+
+        [TestMethod]
+        public void UserAddressPropertyOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            String TestData = "Address";
+            AnOrder.userAddress = TestData;
+            Assert.AreEqual(AnOrder.userAddress, TestData);
+        }
+
+        [TestMethod]
+        public void SoldDatePropertyOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.soldDate = TestData;
+            Assert.AreEqual(AnOrder.soldDate, TestData);
+        }
+    }
+}
